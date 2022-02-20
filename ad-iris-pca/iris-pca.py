7274927282da44e7758a259e608a189670460159
adrien-intern <adrien.degrange@aodocs.com>
2022-02-20 13:04:02 +0100

    running heuristics

--- ad-iris-pca/iris-pca.py
+++ ad-iris-pca/iris-pca.py
@@ -1,3 +1,4 @@
+import numpy
 from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
 
@@ -28,28 +29,38 @@ y0_versicolor = 0
 a_versicolor = .89
 
 
-fig = custom_plotter.plot_projections(X2, Y, iris_data.target_names, 'iris-pca-main2', pca_nb_components,
-                                      additional_display=lambda local_plt:
-                                      (local_plt.axvline(x=x_setosa, color="royalblue"),
-                                       local_plt.axline((x0_versicolor, y0_versicolor), slope=a_versicolor,
-                                                        color="darkorange")),
-                                      display=True)
-
+custom_plotter.plot_projections(X2, Y, iris_data.target_names, 'iris-pca-main2', pca_nb_components,
+                                additional_display=lambda local_plt:
+                                (local_plt.axvline(x=x_setosa, color="royalblue"),
+                                 local_plt.axline((x0_versicolor, y0_versicolor), slope=a_versicolor,
+                                                  color="darkorange")),
+                                display=False)
 
 # Simulating a test data set...
 # This should NOT be the learning data set
 X_test_dataset = X[::-1]
 X3 = pca.fit(X).transform(X_test_dataset)
 
-Y_test_dataset = list()
+Y_test_dataset = numpy.ndarray((len(X_test_dataset)), dtype=int)
 
-for flower in X3:
+for idx, flower in enumerate(X3):
     if flower[0] <= x_setosa:
-        Y_test_dataset.append(0)
+        Y_test_dataset[idx] = 0
 
+    # Flower is Virginica if the scalar product
+    # with a normal vector is > 0
+    elif (flower[0] - x0_versicolor) * a_versicolor + (flower[1] - y0_versicolor) * (-1) > 0:
+        Y_test_dataset[idx] = 2
 
+    # Otherwise, Versicolor
+    else:
+        Y_test_dataset[idx] = 1
 
-
-
-
+custom_plotter.plot_projections(X3, Y_test_dataset, iris_data.target_names, 'iris-pca-test',
+                                pca_nb_components,
+                                additional_display=lambda local_plt:
+                                (local_plt.axvline(x=x_setosa, color="royalblue"),
+                                 local_plt.axline((x0_versicolor, y0_versicolor), slope=a_versicolor,
+                                                  color="darkorange")),
+                                display=True)
 
